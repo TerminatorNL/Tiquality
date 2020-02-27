799b59c2839bf6c299739ac723bbabb7a57ffa29
idnthvn <joshy788@hotmail.com>
2020-02-27 10:38:38 -0500

    Update MixinConfigPlugin.java

--- src/main/java/com/github/terminatornl/tiquality/mixinhelper/MixinConfigPlugin.java
+++ src/main/java/com/github/terminatornl/tiquality/mixinhelper/MixinConfigPlugin.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.CoreModManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.lib.tree.ClassNode;
+import orf.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
